16a180d14d4a77caefeea32ad416df309c96b61f
Andrews Goes <andrews.goes.33@gmail.com>
2024-12-17 08:34:24 -0300

    adicionar transformação com pandas

--- src/transformacao/main.py
+++ src/transformacao/main.py
@@ -23,7 +23,7 @@ df['new_price_cents'] = df['new_price_cents'].fillna(0).astype(float)
 df['reviews_rating_number'] = df['reviews_rating_number'].fillna(0).astype(float)
 
 #Remover parênteses do review_amount
-df['reviews_amount'] = df['reviews_amount'].str.replace('[\(\)]','', regex=True)
+df['reviews_amount'] = df['reviews_amount'].str.replace(r'[\(\)]','', regex=True)
 df['reviews_amount'] = df['reviews_amount'].fillna(0).astype(int)
 
 #tratar os preços como floats e calcular os valores totais
@@ -31,7 +31,7 @@ df['old_price'] = df['old_price_reais'] + df['old_price_cents'] / 100
 df['new_price'] = df['new_price_reais'] + df['new_price_cents'] / 100
 
 #Remover as colunas antigas de preços
-df.drop(columns=['old_price_reais', 'old_price_cents', 'new_price_reais', 'new_price_cents'])
+df = df.drop(columns=['old_price_reais', 'old_price_cents', 'new_price_reais', 'new_price_cents'])
 
 #Conectar ao banco de dados SQLite (ou criar um novo)
 
